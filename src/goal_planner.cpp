ec88999053c4c6854917a5bc99de1f0b69e60f1d
Ole Rößler <ole.nelson.r@gmail.com>
2022-12-10 16:01:33 +0100

    outsourced SubscriberCollection

--- src/goal_planner.cpp
+++ src/goal_planner.cpp
@@ -1,56 +1,65 @@
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <visualization_msgs/Marker.h>
-
 #include <move_base_msgs/MoveBaseGoal.h>
 #include <move_base_msgs/MoveBaseAction.h>
-
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
-
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
-
 #include <actionlib/client/simple_action_client.h>
+#include </home/oleroessler/catkin_ws/src/hwp_goalplanner/src/subscriber_collection.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-class Utils{
-    public: 
-        ros::Subscriber map_sub;
-        ros::Subscriber red_triangle_sub;
-        ros::Subscriber blue_squere_sub;
-
-        //Client ?
-
-};
+/**
+ * * Gets the Map 
+ * Callbackfunction that gets the map from /map
+ * @param map The map  
+*/
 void getMap(nav_msgs::OccupancyGrid map){
     ROS_INFO("Gets Map");
 }
 
-
-void foundRedTriangle(visualization_msgs::Marker){
+/**
+ * * Gets a red triangle  
+ * Callbackfunction that gets a triangle (is executed if a new(?) triangel is found).
+ * @param fire The tiangle represents a fire  
+*/
+void foundRedTriangle(visualization_msgs::Marker fire){
     ROS_INFO("Found red triangle");
 }
-void foundBlueSquare(visualization_msgs::Marker){
+
+/**
+ * * Gets a blue squere  
+ * Callbackfunction that gets a squere (is executed if a new(?) squere is found).
+ * @param human The squere represents a human  
+*/
+void foundBlueSquare(visualization_msgs::Marker human){
     ROS_INFO("Found blue sqaure");
+}
+
+/**
+ * * Creates a filled SubscriberCollection instance
+ * @param nh The nodehandle thats needed for the subscribers
+ * @return A SubscriberCollection
+*/
+SubscriberCollection filledSubCollection(ros::NodeHandle nh){
+    SubscriberCollection subs;
+    //Saves all subscribers in a SubscriberCollection
+    subs.map_sub = nh.subscribe("/map", 10, getMap);
+    subs.red_triangle_sub = nh.subscribe("/red_triangle_pos", 10, foundRedTriangle);
+    subs.blue_squere_sub = nh.subscribe("/blue_square_pos", 10, foundBlueSquare);
+
+    return subs;
 }
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "GoalPlanner");
     ros::NodeHandle nh;
-    Utils util;
+    SubscriberCollection subs = filledSubCollection(nh);
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    //Save all subscribers in the util instance
-    util.map_sub = nh.subscribe("/map", 10, getMap);
-    util.red_triangle_sub = nh.subscribe("/red_triangle_pos", 10, foundRedTriangle);
-    util.blue_squere_sub = nh.subscribe("/blue_square_pos", 10, foundBlueSquare);
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    //setup Client (true causes the client to spin on its own)
+    //sets up Client (true causes the client to spin on its own)
     MoveBaseClient actionClient("/map", true);
 
     //waiting for the server to connect
@@ -58,26 +67,27 @@ int main(int argc, char **argv){
         ROS_INFO("Waiting for the server to come up");
     }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+    //creates goal to sent
     move_base_msgs::MoveBaseGoal goal;
    
-    //configure the goal here
+    /* configure the goal here */
 
     //sending the goal
     ROS_INFO("Sending goal");
     actionClient.sendGoal(goal);
 
+    //waiting for a responce
+    while(!actionClient.waitForResult()){
+        ROS_INFO("Waiting for responce");
+    }
 
-    actionClient.waitForResult();
-
+    //basic responce handling 
     if(actionClient.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
         ROS_INFO("Goal achieved"); 
     }else{
         ROS_INFO("Goal not achieved");
     }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     ros::spin();
 }
