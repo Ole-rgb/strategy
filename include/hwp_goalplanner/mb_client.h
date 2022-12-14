f0278908c65968b5b7e3de47aecb9363614d6d6d
Ole Rößler <ole.nelson.r@gmail.com>
2022-12-14 20:54:21 +0100

    changed imports

--- include/hwp_goalplanner/mb_client.h
+++ include/hwp_goalplanner/mb_client.h
@@ -1,6 +1,5 @@
 #include <ros/ros.h>
 #include <hwp_goalplanner/state.h>
-#include <hwp_goalplanner/subscriber_collection.h>
 #include <move_base_msgs/MoveBaseGoal.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
